59c8d8e253c75c93278000e067e18584289a4d5f
xingzhe <332399398@qq.com>
2018-07-07 00:33:37 +0800

    计算文字防止重叠

--- app/src/main/java/com/example/shiwu/customviewstudy/MainActivity.java
+++ app/src/main/java/com/example/shiwu/customviewstudy/MainActivity.java
@@ -3,8 +3,8 @@ package com.example.shiwu.customviewstudy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.shiwu.customviewstudy.prectise.PieChartView;
-import com.example.shiwu.customviewstudy.prectise.DrawData;
+import com.example.shiwu.customviewstudy.view.DrawData;
+import com.example.shiwu.customviewstudy.view.PieChartView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +24,18 @@ public class MainActivity extends AppCompatActivity {
     private void initData() {
         List<DrawData> list = new ArrayList<>();
 
-        DrawData item0 = new DrawData(10,R.color.C1,"第一项第一项第一项");
-        DrawData item1 = new DrawData(30,R.color.C2,"第二项");
-        DrawData item2 = new DrawData(40,R.color.C3,"第三项");
-        DrawData item3 = new DrawData(10,R.color.C4,"第四项");
-        DrawData item4 = new DrawData(20,R.color.C5,"第五项");
+        DrawData item0 = new DrawData(84,R.color.C1,"第一项");
+        DrawData item1 = new DrawData(1,R.color.C2,"第二项\r\n换行");
+        DrawData item2 = new DrawData(1,R.color.C3,"第三项\n换行");
+        DrawData item3 = new DrawData(1,R.color.C4,"第四项");
+        DrawData item4 = new DrawData(1,R.color.C5,"第五项");
+        DrawData item5 = new DrawData(10,R.color.C6,"第六项");
         list.add(item0);
         list.add(item1);
         list.add(item2);
         list.add(item3);
         list.add(item4);
+        list.add(item5);
 
         mCircleView.setData(list);
     }
--- app/src/main/java/com/example/shiwu/customviewstudy/view/PieChartView.java
+++ app/src/main/java/com/example/shiwu/customviewstudy/view/PieChartView.java
@@ -8,17 +8,15 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.graphics.Xfermode;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.example.shiwu.customviewstudy.R;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,6 +41,11 @@ public class PieChartView extends View {
     private int centerX;
     private int centerY;
 
+    private List<Point> middlePoints;
+    private List<Point> turnPoints;
+
+    private int textMargin;
+
 
     public PieChartView(Context context) {
         this(context, null);
@@ -54,14 +57,14 @@ public class PieChartView extends View {
 
     public PieChartView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        TypedArray ta = context.obtainStyledAttributes(attrs,R.styleable.pieChartStyle,defStyleAttr,0);
-        circleRadius = (int) ta.getDimension(R.styleable.pieChartStyle_pieRadius, 250);
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.pieChartStyle, defStyleAttr, 0);
         ringWidth = (int) ta.getDimension(R.styleable.pieChartStyle_ringWidth, 150);
-        textSize = (int) ta.getDimension(R.styleable.pieChartStyle_describeTextSize, 24);
+        textSize =(int) ta.getDimension(R.styleable.pieChartStyle_describeTextSize, sp2px(context,12));
         ta.recycle();
         resources = context.getResources();
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setStyle(Paint.Style.FILL);
+        textMargin = dp2px(context,10);
     }
 
     @Override
@@ -70,8 +73,9 @@ public class PieChartView extends View {
         if (datas == null) {
             return;
         }
-        centerX = getWidth() / 2;
-        centerY = getHeight() / 2;
+        centerX = getWidth()/ 2;
+        centerY = getHeight()/ 2;
+        circleRadius = (getHeight() -getPaddingTop() - getPaddingBottom())/ 2;
         currentAngle = -90.0f;
         totalNum = 0;
 
@@ -82,20 +86,95 @@ public class PieChartView extends View {
         Bitmap source = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
         Canvas sourceCanvas = new Canvas(source);
 
-        circleRect = new RectF(centerX - circleRadius,centerY - circleRadius, centerX + circleRadius, centerY+ circleRadius);
-
-        for (DrawData data : datas) {
-            drawItem(sourceCanvas, data);
+        circleRect = new RectF(centerX - circleRadius, centerY - circleRadius, centerX + circleRadius, centerY + circleRadius);
+        if (middlePoints != null) {
+            middlePoints.clear();
         }
-
-        Xfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_OUT);
-        mPaint.setXfermode(xfermode);
-        sourceCanvas.drawCircle(centerX, centerY, circleRadius - ringWidth, mPaint);
-        mPaint.setXfermode(null);
+        for (DrawData data : datas) {
+            drawArc(sourceCanvas, data);
+        }
+        drawTextAndLine(canvas);
         canvas.drawBitmap(source, 0, 0, mPaint);
     }
 
-    private void drawItem(Canvas canvas, DrawData data) {
+    private void drawTextAndLine(Canvas canvas) {
+        int leftHeight = 0;
+        int rightHeight = 0;
+        List<Point> left = new ArrayList<>();
+        List<Point> right = new ArrayList<>();
+        for (Point point : middlePoints) {
+            int quadrant = getQuadrant(point);
+            switch (quadrant) {
+                case 1:
+                case 2:
+                    right.add(point);
+                    break;
+                case 3:
+                case 4:
+                    left.add(point);
+                    break;
+            }
+        }
+        mPaint.setTextSize(textSize);
+        mPaint.setColor(getContext().getResources().getColor(R.color.black));
+        int startX,startY,endX,endY;
+        startX = centerX + circleRadius + dp2px(getContext(),10);
+        Path path = new Path();
+        for (int i = 0; i < right.size(); i++) {
+            String title = datas.get(i).getName();
+            Rect titleRect = getTitleSize(title);
+            Point turnPoint = turnPoints.get(i);
+            RectF textRect = new RectF();
+            endX = startX + titleRect.width();
+            startY = (int) (turnPoint.y - (titleRect.height() * 1.0f)/2);
+            endY = startY + titleRect.height();
+            if (startY > rightHeight + textMargin) {
+                rightHeight = endY;
+            } else {
+                startY = rightHeight + textMargin;
+                endY = rightHeight + textMargin + titleRect.height();
+                rightHeight += (titleRect.height() + textMargin);
+            }
+            path.moveTo(right.get(i).x, right.get(i).y);
+            path.lineTo(turnPoint.x, turnPoint.y);
+            path.lineTo(startX -dp2px(getContext(),10),turnPoint.y);
+            path.lineTo(startX,startY + (titleRect.height() * 1.0f)/2);
+            mPaint.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(path, mPaint);
+            textRect.set(startX,startY,endX,endY);
+            mPaint.setStyle(Paint.Style.FILL);
+            canvas.drawText(title, textRect.left, textRect.bottom, mPaint);
+        }
+        for (int i = left.size() - 1; i >=0; i--) {
+            String title = datas.get(right.size() + i).getName();
+            Rect titleRect = getTitleSize(title);
+            Point turnPoint = turnPoints.get(right.size() + i);
+            RectF textRect = new RectF();
+            startX = centerX - circleRadius -dp2px(getContext(),10);
+            endX = startX;
+            startX -= titleRect.width();
+            startY = (int) (turnPoint.y - (titleRect.height() * 1.0f)/2);
+            endY = startY + titleRect.height();
+            if (startY > leftHeight + textMargin) {
+                leftHeight = endY;
+            } else {
+                startY = leftHeight + textMargin;
+                endY = leftHeight + textMargin + titleRect.height();
+                leftHeight += titleRect.height() + textMargin;
+            }
+            path.moveTo(left.get(i).x, left.get(i).y);
+            path.lineTo(turnPoint.x, turnPoint.y);
+            path.lineTo(endX+dp2px(getContext(),10),turnPoint.y);
+            path.lineTo(endX,startY + (titleRect.height() * 1.0f)/2);
+            mPaint.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(path, mPaint);
+            textRect.set(startX,startY,endX,endY);
+            mPaint.setStyle(Paint.Style.FILL);
+            canvas.drawText(title, textRect.left, textRect.bottom, mPaint);
+        }
+    }
+
+    private void drawArc(Canvas canvas, DrawData data) {
         mPaint.setColor(resources.getColor(data.getColor()));
         float sweepAngle = data.getRate() / totalNum * 360;
         if (datas.indexOf(data) == datas.size() - 1)//最后一项直接画满剩余角度
@@ -104,48 +183,34 @@ public class PieChartView extends View {
 
         Point middlePoint = getPoint(currentAngle, sweepAngle, false);
         Point turnPoint = getPoint(currentAngle, sweepAngle, true);
+        if (middlePoints == null) {
+            middlePoints = new ArrayList<>();
+        }
+        middlePoints.add(middlePoint);
 
-        //折线起点的点
-        canvas.drawCircle(middlePoint.x, middlePoint.y, 5, mPaint);
+        if (turnPoints == null) {
+            turnPoints = new ArrayList<>();
+        }
+        turnPoints.add(turnPoint);
 
         //折线path
         Path path = new Path();
         path.moveTo(middlePoint.x, middlePoint.y);
         path.lineTo(turnPoint.x, turnPoint.y);
-
-        //获取文字的长度
-        Rect rect = new Rect();
-        mPaint.setTextSize(textSize);
-        String drawName = data.getName() + (new BigDecimal(data.getRate()).divide(new BigDecimal(totalNum / 100), 2, BigDecimal.ROUND_HALF_UP) + "%");
-        mPaint.getTextBounds(drawName, 0, drawName.length(), rect);
-
-        int nameLength = rect.width();
-        RectF textFect = new RectF();
-        int quadrant = getQuadrant(middlePoint);
-        switch (quadrant) {
-            case 1:
-                path.rLineTo(nameLength, 0);
-                textFect.set(turnPoint.x, turnPoint.y - rect.height(), turnPoint.x + rect.width(), turnPoint.y);
-                break;
-            case 2:
-                path.rLineTo(nameLength, 0);
-                textFect.set(turnPoint.x, turnPoint.y, turnPoint.x + rect.width(), turnPoint.y + rect.height());
-                break;
-            case 3:
-                path.rLineTo(-nameLength, 0);
-                textFect.set(turnPoint.x - rect.width(), turnPoint.y, turnPoint.x, turnPoint.y + rect.height());
-                break;
-            case 4:
-                path.rLineTo(-nameLength, 0);
-                textFect.set(turnPoint.x - rect.width(), turnPoint.y - rect.height(), turnPoint.x, turnPoint.y);
-                break;
-        }
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(1);
         canvas.drawPath(path, mPaint);
         mPaint.setStyle(Paint.Style.FILL);
-        canvas.drawText(drawName, textFect.left, textFect.bottom, mPaint);
         currentAngle += sweepAngle;
+    }
+
+    private Rect getTitleSize(String str) {
+        //获取文字的长度
+        Rect rect = new Rect();
+        TextPaint paint = new TextPaint();
+        paint.setTextSize(textSize);
+        paint.getTextBounds(str, 0, str.length(), rect);
+        return rect;
     }
 
     //获取折线起点或者转折点
@@ -158,11 +223,11 @@ public class PieChartView extends View {
         int middleY;
         double radian = middleAngle * 2 * Math.PI / 360;
         if (!isTurningPoint) {
-            middleX = (int) (circleX + Math.cos(radian) * (circleRadius + 10));
-            middleY = (int) (circleY + Math.sin(radian) * (circleRadius + 10));
+            middleX = (int) (circleX + Math.cos(radian) * (circleRadius - dp2px(getContext(),10)));
+            middleY = (int) (circleY + Math.sin(radian) * (circleRadius - dp2px(getContext(),10)));
         } else {
-            middleX = (int) (circleX + Math.cos(radian) * (circleRadius + 40));
-            middleY = (int) (circleY + Math.sin(radian) * (circleRadius + 40));
+            middleX = (int) (circleX + Math.cos(radian) * (circleRadius + dp2px(getContext(),10)));
+            middleY = (int) (circleY + Math.sin(radian) * (circleRadius + dp2px(getContext(),10)));
         }
 
         return new Point(middleX, middleY);
@@ -187,5 +252,35 @@ public class PieChartView extends View {
     public void setData(List<DrawData> datas) {
         this.datas = datas;
         invalidate();
+    }
+    /**
+     * convert dp to its equivalent px
+     *
+     * 将dp转换为与之相等的px
+     */
+    public static int dp2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    /**
+     * convert sp to its equivalent px
+     *
+     * 将sp转换为px
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+
+    /**
+     * convert px to its equivalent sp
+     *
+     * 将px转换为sp
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
     }
 }
